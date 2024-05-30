c2c3d4e07d70e2c2f9510c2007ab343a9753926d
Orlando Enamorado Rosabal <rosabal.orlando69@gmail.com>
2024-05-30 03:16:53 +0200

    chore: Add RPC exception handling functions

--- src/common/errors/exception-errors.ts
+++ src/common/errors/exception-errors.ts
@@ -3,3 +3,15 @@ import { ObjectId } from 'mongoose';
 export function rpcExceptionNoDataFound(id: string | number | ObjectId) {
   return `No data found for id: ${id}`;
 }
+
+export function rpcExceptionFindOne() {
+  return `Error find data`;
+}
+
+export function rpcExceptionCreatedData() {
+  return `Error creating data`;
+}
+
+export function rpcExceptionFindData() {
+  return `Error find data`;
+}
